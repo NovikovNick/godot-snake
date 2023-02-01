26b286f4ba80a6e8b3ad9ae6ee4dcb416135f425
nnovikov <nicknovikov10@gmail.com>
2023-02-01 20:48:33 +0700

    finish basic GDNative C++ tutorial

---
+++ project/Main.gd
@@ -0,0 +1,27 @@
+extends Node
+
+const Simple = preload("res://bin/simple.gdns")
+
+onready var simple_instance = Simple.new()
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Sprite_position_changed(node, new_pos):
+	print("The position of " + node.name + " is now " + str(new_pos))
+
+
+func _on_SampleButton_pressed():
+	$Label.text = "C++ code says: " + simple_instance.get_data()
