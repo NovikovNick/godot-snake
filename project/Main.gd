4d7ba2acce58af8a142b1cd5eacc865bb5d9a644
nnovikov <nicknovikov10@gmail.com>
2023-02-09 16:19:01 +0700

    add resources and update godot project script

--- project/Main.gd
+++ project/Main.gd
@@ -2,19 +2,33 @@ extends Node
 
 const Board = preload("res://bin/snake_board.gdns")
 
-onready var board_instance = Board.new()
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+onready var board = Board.new()
+var t0;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	board_instance.start();
-	print("Apple => " + str(board_instance.get_cell(1,2)));
-
+	board.start();
+	t0 = 0;
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+
+	if Input.is_action_pressed("move_up"):
+		board.update_input(1)
+	if Input.is_action_pressed("move_left"):
+		board.update_input(2)
+	if Input.is_action_pressed("move_right"):
+		board.update_input(3)
+	if Input.is_action_pressed("move_down"):
+		board.update_input(4)
+	
+	t0 += delta
+	if(t0 > 0.2):
+		board.move(0)
+		#board.move(1)
+		t0 = 0;
+	
+	for row in range(10):
+		for col in range(10):
+			$SnakeTileMap.set_cell(col, row, board.get_cell(col, row));
+	
